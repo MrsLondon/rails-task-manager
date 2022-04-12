92f3efaa8e29b90a5036683a5f36df499e047aa7
Ayat AbuHaj <abuhajayat@gmail.com>
2022-04-12 02:24:50 +0200

    task view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,10 +10,21 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
 end
 
-
 private
+
 def task_params
   params.require(:task).permit(:title, :details, :completed)
 end
